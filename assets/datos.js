35e0998b9bde5a46aa6b3f2ce206acc08f6eae81
estudiemos <estudiemos@proton.me>
2023-03-17 22:16:46 +0100

    limpieza js

--- assets/datos.js
+++ assets/datos.js
@@ -1,3 +1,57 @@
+//consonantes = "bcdfghjklmnñpqrstvwxyz"
+//vocales = "aeiou"
+silabas = `a e i o u
+            ba be bi bo bu
+            ca ce ci co cu
+            cha che chi cho chu
+            da de di do du
+            fa fe fi fo fu
+            ga ge gi go gu
+            gue gui
+            ha he hi ho hu
+            ja je ji jo ju
+            ka ke ki ko ku
+            la le li lo lu
+            ma me mi mo mu
+            na ne ni no nu
+            ña ñe ñi ño ñu
+            pa pe pi po pu
+            que qui
+            ra re ri ro ru
+            sa se si so su
+            ta te ti to tu
+            va ve vi vo vu
+            xa xe xi xo xu                    
+            ya ye yi yo yu
+            za zo zu`.replace(/\s{2,}/g, ' ').replace(/\n/g, " ").split(" ")
+
+pronunciaciones = {};
+pronunciaciones['ba'] = "bá"
+pronunciaciones['be'] = "bé"
+pronunciaciones['bi'] = "bí"
+pronunciaciones['ci'] = "cì"
+pronunciaciones['cu'] = "cù"
+pronunciaciones['chu'] = "chù"
+pronunciaciones['ga'] = "gá"
+pronunciaciones['ge'] = "gë"
+pronunciaciones['go'] = "gò"
+pronunciaciones['gu'] = "gú"
+pronunciaciones['ha'] = "a"
+pronunciaciones['he'] = "e"
+pronunciaciones['hi'] = "i"
+pronunciaciones['ho'] = "o"
+pronunciaciones['hu'] = "u"
+pronunciaciones['je'] = "ge"
+pronunciaciones['ji'] = "gi"
+pronunciaciones['ju'] = "jú"
+pronunciaciones['pu'] = "pú"
+pronunciaciones['so'] = "sò"
+pronunciaciones['to'] = "tò"
+pronunciaciones['xa'] = "xà"
+pronunciaciones['xe'] = "xè"
+pronunciaciones['za'] = "zä"
+
+emojis = {};
 emojis['a']=['abeja','']
 emojis['e']=['erizo','']
 emojis['i']=['imán','']
--- assets/funciones.js
+++ assets/funciones.js
@@ -1,3 +1,4 @@
+/*
 function pronuncia(silaba) {
     let pronunciacion = silaba in pronunciaciones ? pronunciaciones[silaba] : silaba
     const utterThis = new SpeechSynthesisUtterance(pronunciacion);
@@ -9,33 +10,39 @@ function pronuncia(silaba) {
     console.log(utterThis.voice);
     synth.speak(utterThis);
 }
+*/
 
 async function escuchar() {
-    await pronunciar()
+    let texto = document.getElementById("texto").textContent.toLowerCase()
+    if (texto in pronunciaciones) {
+        await pronunciar(pronunciaciones[texto])
+    } else { pronunciar(texto) }
     await dormir(500)
-    await pronunciar_palabra()
-    await dormir(500)
+    
+    if (texto in emojis) {        
+        await pronunciar(emojis[texto][0])
+        await dormir(500)
+    }
     mostrar_elemento('acertaste')
 
 }
 
-async function pronunciar() {
-    let silaba = document.getElementById("texto").textContent.toLowerCase()
-    let pronunciacion = silaba in pronunciaciones ? pronunciaciones[silaba] : silaba
+async function pronunciar(pronunciacion) {
     const utterThis = new SpeechSynthesisUtterance(pronunciacion);
     utterThis.pitch = 1;
     utterThis.lang = 'es-ES';
     utterThis.rate = 0.9;
-    const voices = speechSynthesis.getVoices();
-    utterThis.voice = voices[11];
+    //const voices = speechSynthesis.getVoices();
+    //utterThis.voice = voices[11];
+    utterThis.voice = speechSynthesis.getVoices().find(voice => /Google español/.test(voice.name));
     console.log(utterThis.voice);
-    document.getElementById("voice").textContent = utterThis.voice.lang
+    document.getElementById("voice").textContent = utterThis.voice.name
     synth.speak(utterThis);
     return new Promise(resolve => {
         utterThis.onend = resolve;
     });
 }
-
+/*
 async function pronunciar_palabra() {
     let silaba = document.getElementById("texto").textContent.toLowerCase()
     let pronunciacion = silaba in emojis ? emojis[silaba][0] : pronunciaciones[silaba]
@@ -44,14 +51,20 @@ async function pronunciar_palabra() {
     utterThis.lang = 'es-ES';
     utterThis.rate = 0.9;
     const voices = speechSynthesis.getVoices();
-    utterThis.voice = voices[11];
+    for (let i = 0; i < voices.length; i++) {
+        console.log(voices[i])
+    }
+
+    //utterThis.voice = voices[11];
+    utterThis.voice = speechSynthesis.getVoices().find(voice => /Google español/.test(voice.name));
     console.log(utterThis.voice);
-    document.getElementById("voice").textContent = utterThis.voice.lang
+    document.getElementById("voice").textContent = utterThis.voice.name
     synth.speak(utterThis);
     return new Promise(resolve => {
         utterThis.onend = resolve;
     });
 }
+*/
 
 function dormir(ms) {
     return new Promise(
@@ -74,60 +87,6 @@ async function mostrar_elemento(identificador) {
 }
 
 const synth = window.speechSynthesis;
-//consonantes = "bcdfghjklmnñpqrstvwxyz"
-//vocales = "aeiou"
-pronunciaciones = {};
-pronunciaciones['ba'] = "bá"
-pronunciaciones['be'] = "bé"
-pronunciaciones['bi'] = "bí"
-pronunciaciones['ci'] = "cì"
-pronunciaciones['cu'] = "cù"
-pronunciaciones['chu'] = "chù"
-pronunciaciones['ga'] = "gá"
-pronunciaciones['ge'] = "gë"
-pronunciaciones['go'] = "gò"
-pronunciaciones['gu'] = "gú"
-pronunciaciones['ha'] = "a"
-pronunciaciones['he'] = "e"
-pronunciaciones['hi'] = "i"
-pronunciaciones['ho'] = "o"
-pronunciaciones['hu'] = "u"
-pronunciaciones['je'] = "ge"
-pronunciaciones['ji'] = "gi"
-pronunciaciones['ju'] = "jú"
-pronunciaciones['pu'] = "pú"
-pronunciaciones['so'] = "sò"
-pronunciaciones['to'] = "tò"
-pronunciaciones['xa'] = "xà"
-pronunciaciones['xe'] = "xè"
-pronunciaciones['za'] = "zä"
-
-emojis = {};
-
-silabas = `a e i o u
-            ba be bi bo bu
-            ca ce ci co cu
-            cha che chi cho chu
-            da de di do du
-            fa fe fi fo fu
-            ga ge gi go gu
-            gue gui
-            ha he hi ho hu
-            ja je ji jo ju
-            ka ke ki ko ku
-            la le li lo lu
-            ma me mi mo mu
-            na ne ni no nu
-            ña ñe ñi ño ñu
-            pa pe pi po pu
-            que qui
-            ra re ri ro ru
-            sa se si so su
-            ta te ti to tu
-            va ve vi vo vu
-            xa xe xi xo xu                    
-            ya ye yi yo yu
-            za zo zu`.replace(/\s{2,}/g, ' ').replace(/\n/g, " ").split(" ")
 
 function bucle() {
     /*const silabas = []
